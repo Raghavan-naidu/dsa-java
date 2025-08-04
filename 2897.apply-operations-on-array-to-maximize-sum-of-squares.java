f4fdfa153de82212f936d9466b84a23633c373ac
Raghavan <raghavannaidu.official@gmail.com>
2025-08-04 20:51:51 +0530

    first commit

---
+++ 2897.apply-operations-on-array-to-maximize-sum-of-squares.java
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode id=2897 lang=java
+ *
+ * [2897] Apply Operations on Array to Maximize Sum of Squares
+ */
+
+// @lc code=start
+
+import java.util.List;
+
+class Solution {
+    public int maxSum(List<Integer> nums, int k) {
+        return k;
+        
+    }
+}
+// @lc code=end
+
